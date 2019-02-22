5335fe9ffe6ad69da0891c7260d92446b6fea92f
Luke Harby <luke.harby@contractor.isbanuk.com>
2019-02-22 21:50:09 +0000

    init new functionality

--- README.md
+++ README.md
@@ -2,28 +2,21 @@
 
 Application to toggle css files and store the value in localStorage for retrieval later
 
-Inside index.html we set several wrappers and one 'empty' css link which we can populate when the client selects a css option.
+At runtime a function will execute and strip out any css files and style tags and set a css file according 
 
 ```
-<!-- put any inherited stylesheets in here (which should NOT be switched) -->
-<span class="constant">
-  <link rel="stylesheet" type="text/css" href="css/constant.css">
-</span>
-
-<!-- put any stylesheets which can be switched off in here -->
-<span class="common">
-  <link rel="stylesheet" type="text/css" href="css/common.css">
-  <link rel="stylesheet" type="text/css" href="css/other-styles.css">
-  <link rel="stylesheet" type="text/css" href="css/third-style.css">
-</span>
-
-<!-- this is the switcher stylesheet, it is changed via javascript using a select input -->
+*<!-- this is the switcher stylesheet, it is changed via javascript using a select input -->*
 <link rel="stylesheet" class="switcher" type="text/css" href="" />
 ```
 
 Populate a select with all the stylesheets that the client can switch too.
 
-The value of the first option MUST be 'default', in order for the javascript to work. If the client selects 'default' the switched css will be removed as well as clearing the css value from localStorage and resetting any other css files we have hidden when the client selected one of the options. The values of the other select options must match the name of a css file stored in a common css folder.
+The value of the first option MUST be 'default', in order for the javascript to work. If the client selects 'default' the switched css will be removed as well as clearing the css value from localStorage and reinsating any other css files we have removed when the client selected one of the options. The values of the other select options must match the name of a css file stored in a common css folder. In this example I have set them to:
+```
+css/css-switcher/
+```
+
+Example of the dropdown:
 
 ```
 <select class="trigger">
--- js/cssSwitcher.js
+++ js/cssSwitcher.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-    const changeCSS = function(){
-        const common = $('.common');
+    const setCSS = function(){
         const switcher = $('.switcher');
+        const cssTags = $('style, [rel="stylesheet"]').not(switcher);
+        const cssPath = '../css/switcher-css/';
         const trigger = $('.trigger');
-        const cssPath = 'css/'
         const empty = '';
         // set the css item in localStorage
         let cssIsSet = localStorage.getItem('chCSS');
@@ -12,37 +12,32 @@ $(document).ready(function(){
             trigger.on('change',function(){
                 // get current value from select
                 const thisCSS = $(this).val();
-                // if the default is selected reset
-                // all css values and clear localStorage
+                // if the default is selected remove
+                // all css and style tags and clear localStorage
                 if($(this).val() == 'default'){
-                    uncomment(common);
                     switcher.attr('href', empty);
                     localStorage.clear();
+                    // reload the page
+                    location.reload();
                 }else{
                     // otherwise set the new css value
                     // and store it in localStorage so
                     // we can retrieve it later
-                    uncomment(common);
-                    comment(common);
+                    // remove all css and style tags
+                    // except the switcher
+                    cssTags.remove();
                     switcher.attr('href', cssPath + thisCSS + '.css');
                     localStorage.setItem('chCSS', thisCSS);
                 }
             });
         }();
-        // wrap html in comment function
-        function comment(element){
-            element.html('<!--' + element.html() + '-->')
-        };
-        // unwrap html from comment function
-        function uncomment(element){
-          element.html(element.html().replace('<!--','').replace('-->',''))
-        };
         // check if client has set css at runtime
         // if so populate the css value
         const chCSSSet = function(){
             if(cssIsSet !== null && typeof cssIsSet !== 'undefined' && cssIsSet !== ''){
-                comment(common);
-                switcher.attr('href', cssPath + cssIsSet + '.css');
+                // remove all css and style tags
+                cssTags.remove();
+                switcher.attr('href', cssPath +cssIsSet+ '.css');
                 trigger.find('option').each(function(){
                     if($(this).val() == cssIsSet){
                         $(this).attr('selected', 'selected');
@@ -52,4 +47,4 @@ $(document).ready(function(){
         }();
     }();
 
-}); 
+});
